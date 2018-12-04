557c0e5512ace648dde59ec5f8f70138c74fec77
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-12-04 09:43:35 -0800

    Generate index page for each tag
    Note that {{ content }} is removed.
    This causes each successive post to render
    the content of the previous post."

---
+++ source/_plugins/tag_page.rb
@@ -0,0 +1,24 @@
+module Jekyll
+    class TagPage < Page
+        def initialize(site, base, tag)
+            @site = site
+            @base = base
+            @tag = tag
+            @name = "#{tag}.html"
+
+            self.process(@name)
+            self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
+            self.data['tag'] = @tag
+        end
+    end
+
+    class TagPageGenerator < Generator
+        safe true
+
+        def generate(site)
+            site.tags.each_key do |tag|
+                site.pages << TagPage.new(site, site.source, tag)
+            end
+        end
+    end
+end
